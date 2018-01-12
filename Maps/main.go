8062b450a1d6bbe291eb72a5673948d739615a83
Brian Sweeney <brian.sweeney@ibm.com@Brians-MBP.home>
2018-01-12 15:29:54 -0500

    method 3 of map creation; updating maps;

--- Maps/main.go
+++ Maps/main.go
@@ -3,7 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	var colors map[string]string
+	colors := make(map[string]string)
+
+	colors["white"] = "#ffffff"
+
+	delete(colors, "white")
 
 	fmt.Println(colors)
 }
