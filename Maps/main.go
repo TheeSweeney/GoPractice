17286e96010ec434594a9b7d7cf20d27adc096b7
Brian Sweeney <brian.sweeney@ibm.com@Brians-MBP.home>
2018-01-12 15:22:51 -0500

    map creation method 2;

--- Maps/main.go
+++ Maps/main.go
@@ -3,11 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		//we are declaring a map where all keys are type sting, and all values are type string
-		"red":   "#ff0000",
-		"green": "#4bf745",
-	} //as with structs, maps must end with comma
+	var colors map[string]string
 
 	fmt.Println(colors)
 }
